4d62cf8621df310267b1487b7c5a0e7528e654f9
TheOneWhoWill <norikrafikov3456@gmail.com>
2021-07-28 14:42:13 -0400

    Fixed Input Label Hover Bug

--- client/src/Components/Input.js
+++ client/src/Components/Input.js
@@ -3,8 +3,14 @@ import React from 'react'
 function Input(props) {
 	return (
 		<div className="input">
-			<input ref={props.inputRef} type={props.type} id={props.type} placeholder="  c"/>
-			<label for={props.type}>{props.name}</label>
+			<input
+				placeholder=" "
+				className="inputFeild"
+				ref={props.inputRef}
+				type={props.type}
+				id={props.type}
+			/>
+			<label className="formLabel" for={props.type}>{props.name}</label>
 		</div>
 	)
 }

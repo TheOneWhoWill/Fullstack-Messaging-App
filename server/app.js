a4d632fdfe756a37f53b0dbe7e6b4cf85ba54b67
TheOneWhoWill <norikrafikov3456@gmail.com>
2021-07-30 22:04:55 -0400

    Forgot to Connect to DB, oops!

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 import admin from 'firebase-admin';
 import compression from 'compression';
 import authRouter from './routes/auth.js';
@@ -10,10 +11,17 @@ dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 2000;
+const dbURL = process.env.DBKey;
 
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount)
 })
+
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log('Connected to db')
+  })
+  .catch((error) => console.log(error));
 
 app.use(compression());
 app.use(express.json())

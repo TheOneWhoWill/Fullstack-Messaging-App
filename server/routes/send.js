e6c8427074c85245ddf49f38b2375ede9d214b0b
TheOneWhoWill <norikrafikov3456@gmail.com>
2021-07-30 21:24:52 -0400

    Created a Backend Request for sending messages

--- server/routes/send.js
+++ server/routes/send.js
@@ -1,4 +1,20 @@
 import express from 'express';
-import Message from '../models/message.js'
+import Message from '../models/message.js';
+import { verifyIDToken } from '../middleware/auth.js';
+
 const router = express.Router()
 
+// Send Message
+router.post('/send/message', verifyIDToken, (req, res) => {
+	const message = {
+    sender: req.body.sender,
+		recipient: {
+			type: 'user',
+			uid: req.body.recipient
+		},
+		body: req.body.msg
+  }
+	Message.create(message);
+})
+
+export default router

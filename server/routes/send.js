c8ace5a1f31dfd1ef41e9d3bb453225b511a8386
TheOneWhoWill <norikrafikov3456@gmail.com>
2021-08-06 14:11:47 -0400

    Changed how messages are sent

--- server/routes/send.js
+++ server/routes/send.js
@@ -7,12 +7,15 @@ const router = express.Router()
 // Send Message
 router.post('/message', verifyIDToken, (req, res) => {
 	const message = {
-    sender: req.body.sender,
+    sender: {
+			uid: req.body.sender,
+			name: req.body.senderName
+		},
 		recipient: {
 			type: 'user',
 			uid: req.body.recipient
 		},
-		timestamp: Math.round((new Date()).getTime() / 1000),
+		timestamp: Math.round((new Date()).getTime()),
 		body: req.body.msg,
 		members: [req.body.sender, req.body.recipient]
   }
